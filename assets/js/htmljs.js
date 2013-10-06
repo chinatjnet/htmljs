3c3e72b3958c331e28a5a8514ee835c2417c3e8f
xinyu198736 <xinyu198736@gmail.com>
2013-10-06 13:01:58 +0800

    细节优化

--- assets/js/htmljs.js
+++ assets/js/htmljs.js
@@ -77,5 +77,58 @@ HtmlJS.util = function(){
 $(document).ready(function(){
   $(".new-function").each(function(i,div){
     $(div).append($("<span class='label label-danger new-function-label' data-original-title='"+$(div).attr("data-t")+"'>New!<i></i></span>").tooltip({placement:"bottom"}))
-  })
-})
+  });
+// 获取一个元素的所有css属性的patch, $(el).css()
+        jQuery.fn.css2 = jQuery.fn.css;
+        jQuery.fn.css = function() {
+            if (arguments.length) return jQuery.fn.css2.apply(this, arguments);
+            var attr = ['font-family','font-size','font-weight','font-style','color',
+                'text-transform','text-decoration','letter-spacing', 'box-shadow',
+                'line-height','text-align','vertical-align','direction','background-color',
+                'background-image','background-repeat','background-position',
+                'background-attachment','opacity','width','height','top','right','bottom',
+                'left','margin-top','margin-right','margin-bottom','margin-left',
+                'padding-top','padding-right','padding-bottom','padding-left',
+                'border-top-width','border-right-width','border-bottom-width',
+                'border-left-width','border-top-color','border-right-color',
+                'border-bottom-color','border-left-color','border-top-style',
+                'border-right-style','border-bottom-style','border-left-style','position',
+                'display','visibility','z-index','overflow-x','overflow-y','white-space',
+                'clip','float','clear','cursor','list-style-image','list-style-position',
+                'list-style-type','marker-offset'];
+            var len = attr.length, obj = {};
+            for (var i = 0; i < len; i++) 
+                obj[attr[i]] = jQuery.fn.css2.call(this, attr[i]);
+            return obj;
+        };
+$('textarea.expand').keyup(function () {
+          var t = $(this);
+          if (!this.justifyDoc) {
+              this.justifyDoc = $(document.createElement('div'));
+
+              // copy css
+              this.justifyDoc.css(t.css()).css({
+                  'display'   :   'none',        // you can change to none
+                  'word-wrap' :   'break-word',
+                  'min-height':   t.height(),
+                  'height'    :   'auto'
+              }).insertAfter(t.css('overflow-y', 'hidden'));
+          }
+
+          var html = t.val().replace(/&/g, '&amp;')
+              .replace(/</g, '&lt;')
+              .replace(/>/g, '&gt;')
+              .replace(/'/g, '&#039;')
+              .replace(/"/g, '&quot;')
+              .replace(/ /g, '&nbsp;')
+              .replace(/((&nbsp;)*)&nbsp;/g, '$1 ')
+              .replace(/\n/g, '<br />')
+              .replace(/<br \/>[ ]*$/, '<br />-')
+              .replace(/<br \/> /g, '<br />&nbsp;')+"<br/><br/><br/><br/><br/>";
+
+          this.justifyDoc.html(html);
+          t.height(this.justifyDoc.height());
+      });
+});
+
+
